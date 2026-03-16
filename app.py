a2153f10f136846594e4b1ea0c061d7e4236a81e
priyyaa1103-netizen <priyyaa1103@gmail.com>
2026-03-16 20:01:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -927,6 +927,7 @@ def logout():
     session.clear()
     return redirect('/')
 
+# 🔥 RENDER.COM PORT FIX 🔥
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
